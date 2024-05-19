7e05c098cc7b48465f3750a9d11c7cdb364f9df2
Timo Krazov <83912804+TimoKrazov@users.noreply.github.com>
2024-05-19 15:35:05 +0800

    Update Universal_parser.py

--- NEW-PARS/pages/Universal_parser.py
+++ NEW-PARS/pages/Universal_parser.py
@@ -7,11 +7,6 @@ st.set_page_config(
 )
 
 st.markdown("""
-            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-            <span class="material-symbols-outlined">
-                transgender
-            </span>
             <style>
                 body {
                     background-color: rgb(0,80,78);
