8c50049f9f7d60c9d4b68a0f78d2c6984c5e0e04
Timo Krazov <83912804+TimoKrazov@users.noreply.github.com>
2024-05-19 15:26:19 +0800

    Update Universal_parser.py

--- NEW-PARS/pages/Universal_parser.py
+++ NEW-PARS/pages/Universal_parser.py
@@ -1,18 +1,16 @@
 import streamlit as st
 import pandas as pd
-from back import backend
+import backend
+
 st.set_page_config(
     page_title="Универсальный парсер",
-    page_icon= "🏳️‍🌈", layout="wide"
+    page_icon="🏳️‍🌈", layout="wide"
 )
 
 st.markdown("""
-            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-            <span class="material-symbols-outlined">
-                transgender
-            </span>
             <style>
+            /*Общие настройки*/
+                /*Фон тела (Вне основного фона)*/
                 body {
                     background-color: rgb(0,80,78);
                 }
@@ -20,80 +18,78 @@ st.markdown("""
                 .main {
                     background-color: rgb(230, 230, 230);
                 }
-                [data-testid="stHeader"]{
+                /*Шапка*/
+                .st-emotion-cache-18ni7ap{
                     background: rgb(128,128,128);
                 }
-                [data-testid="StyledLinkIconContainer"]{
+                .st-emotion-cache-1avcm0n{
+                    background: rgb(128,128,128);
+                }
+                /*Заголовок*/
+                .st-emotion-cache-10trblm{
                     color: rgb(49, 51, 63);
                 }
                 /*Текст*/
-                [data-testid="stMarkdown"] {
+                .new_text {
                     color: rgb(49, 51, 63);
                     font-size:20px;
                 }
-                /*фон закрытия бок панели*/
-                [data-testid="stApp"] {
-                    background-color: rgb(230, 230, 230);
-                    color: rgb(240 73 35);
-                }
-                [data-testid="stSidebar"] {
+                .st-emotion-cache-1cypcdb{
                     background-color: rgb(0,80,78);
                     top: 0px;
                     height: 100% !important;
                 }
+                /*фон закрытия бок панели*/
+                .stApp {
+                    background-color: rgb(179, 179, 179);
+                }
+                /*цвет бок панели закрытия*/    
+                .st-emotion-cache-rawifx  {
+                    background-color: rgb(0,80,78);
+                }
                 /*цвет кнопки после зажатия*/
-                [data-testid="stSidebarNavLink"] {
+                .st-emotion-cache-1uy0bt2:active {
+                    background: rgb(179, 179, 179);
+                }
+                .st-emotion-cache-1uy0bt2:visited{
+                    background: rgb(240, 73, 35);
+                }
+                /*Остальные кнопки*/
+                .st-emotion-cache-1m6wrpk{
+                    color: rgb(240,73,35);
+                }
+                /*кнопочки*/
+                .eczjsme6 {
                     background: rgb(0,80,78);
                 }
-                [class="st-emotion-cache-1m6wrpk eczjsme5"] {
-                    color: rgb(240,73,35);
+                .st-emotion-cache-1we6k59:hover {
+                    background: rgb(179,179,179);
+                    color: rgb(240, 73, 35) !important;
                 }
-                [data-testid="stSidebarNavLink"].st-emotion-cache-18l0hbk:hover {
-                    background: rgb(179, 179, 179);
-                    color: rgb(240, 73, 35)
-                }
-                [data-testid="stSidebarNavLink"].st-emotion-cache-nziaof:visited{
+                .st-emotion-cache-1uy0bt2:hover {
                     background: rgb(240, 73, 35);
                 }
-                [data-testid="stSidebarNavLink"].st-emotion-cache-nziaof:hover{
-                    background: rgb(240, 73, 35);
-                }
-                [class="st-emotion-cache-1m6wrpk eczjsme5"] {
-                    color: rgb(240,73,35);
-                }
-                .eczjsme5{
-                    color:rgb(255, 255, 255);
-                }
-                [class="st-emotion-cache-17lntkn eczjsme5"]{
-                    color: rgb(240,73,35);
-                }
-                [class="st-emotion-cache-1m6wrpk eczjsme5"]{
-                    color: rgb(240,73,35);
-                }
-                [data-testid="stSidebarNavLink"].st-emotion-cache-1we6k59:hover {
-                    background: rgb(179, 179, 179);
-                    color: rgb(240, 73, 35)
-                }
-                [data-testid="stSidebarNavLink"].st-emotion-cache-1uy0bt2:visited{
-                    background: rgb(240, 73, 35);
+                /*цвет элементов стримлита*/
+                .st-emotion-cache-13k62yr {
+                    color: rgb(240 73 35);
                 }
                 /*стрелка открытия бок панели*/
-                [data-testid="baseButton-headerNoPadding"] {
+                .st-emotion-cache-1pbsqtx {
                     color: rgb(240, 73, 35);
                 }
-                [data-testid="stSidebarNavLink"].st-emotion-cache-1uy0bt2:hover{
-                    background: rgb(240, 73, 35);
+                .st-emotion-cache-a7yjq {
+                    background-color: rgb(0,80,78);
                 }
             </style>
         """, unsafe_allow_html=True)
 st.markdown("""
             <style>
                 /*Текст загрузки файлов*/
-                [data-testid="stWidgetLabel"] {
+                .st-emotion-cache-sh2krr {
                     color: rgb(49, 51, 63);
                 }
                 /*Полоса загрузки файлов*/
-                [data-testid="stFileUploaderDropzone"] {
+                .e1b2p2ww15 {
                     background-color: rgb(153, 153, 153);
                     color: rgb(49, 51, 63);
                 }
@@ -120,12 +116,18 @@ st.markdown("""
                     background-color: rgb(240, 73, 35);
                     color: rgb(250, 250, 250);
                 }
-                [data-testid="stNumberInputContainer"]{
-                    border-bottom-color:rgb(255, 73, 35);
-                    border-top-color: rgb(255, 73, 35);
-                    border-right-color: rgb(255, 73, 35);
-                    border-left-color: rgb(255, 73, 35);
-                    background: rgb(255, 73, 35);
+                /*границы*/
+                .st-cq {
+                    border-bottom-color:rgb(255, 75, 75);
+                    border-top-color: rgb(255, 75, 75);
+                    border-right-color: rgb(255, 75, 75);
+                    border-left-color: rgb(255, 75, 75);
+                }
+                .st-ba {
+                    border-bottom-color:rgb(255, 75, 75);
+                    border-top-color: rgb(255, 75, 75);
+                    border-right-color: rgb(255, 75, 75);
+                    border-left-color: rgb(255, 75, 75);
                 }
                 .st-emotion-cache-15wihvi, .st-emotion-cache-15wihvi:active, .st-emotion-cache-15wihvi:focus-visible {
                     background: rgb(240,73,35) !important;
@@ -138,23 +140,23 @@ st.markdown("""
                 .st-emotion-cache-oteskg:hover:enabled, .st-emotion-cache-oteskg:focus:enabled {
                     background:rgb(153, 153, 153);
                     color: rgb(0,80,78);
+                    transition: none 0s ease 0s;
+                    outline: none;
                 }
                 /*текст ограничений*/
                 .e1nzilvr5 {
                     color: rgb(240,73,35);
                 }
-                
-                [data-testid="stNumberInput-StepDown"]{
-                    border: 2px solid rgb(240, 73, 35);
-                    border-radius: 5px;
-                    color: rgb(240, 73, 35);
-                    background: rgb(0,80,78);
+                /*+ и -*/
+                .st-emotion-cache-oteskg {
+                    background: rgb(153, 153, 153);
                 }
-                [data-testid="stNumberInput-StepUp"]{
-                    border: 2px solid rgb(240, 73, 35);
-                    border-radius: 5px;
-                    color: rgb(240, 73, 35);
-                    background: rgb(0,80,78);
+                /*текст +-*/
+                .st-emotion-cache-f2zl0u{
+                    color: rgb(0,80,78);
+                }
+                .st-emotion-cache-14zer8g{
+                    color: rgb(0,80,78);
                 }
             </style>
             """, unsafe_allow_html=True)
@@ -162,8 +164,7 @@ st.markdown("""
 st.title('Универсальный парсер Excel-файлов')
 
 
-
-
+#Некая типо логика
 def get_files() -> list:
     uploaded_files = st.file_uploader("Загрузите файлы Excel", type=["xls", "xlsx"], accept_multiple_files=True)
     return uploaded_files
@@ -198,30 +199,26 @@ if files:
         else:
             backend.add_selected_range(df, selected_ranges)
             st.session_state["selected_ranges"] = selected_ranges
-
-            if st.button("Получить данные для выбранных диапазонов"):
-                all_selected_ranges = backend.get_selected_ranges(df, selected_ranges)
-                if len(all_selected_ranges) == 1:
-                    st.write("Выбранный диапазон:")
-                else:
-                    st.write("Выбранные диапазоны:")
-                for selected_data in all_selected_ranges:
-                    st.write(selected_data)
-                # Возможность скачать результат в виде csv-файла
-                sd = backend.convert_to_csv(all_selected_ranges)
-                download = st.download_button(
-                    label="Download data as CSV",
-                    data=sd,
-                    file_name='Результат.csv',
-                    mime='text/csv'
-                )
-
             if selected_ranges:
+                if st.button("Получить данные для выбранных диапазонов"):
+                    all_selected_ranges = backend.get_selected_ranges(df, selected_ranges)
+                    if len(all_selected_ranges) == 1:
+                        st.write("Выбранный диапазон:")
+                    else:
+                        st.write("Выбранные диапазоны:")
+                    for selected_data in all_selected_ranges:
+                        st.write(selected_data)
+                    # Возможность скачать результат в виде csv-файла
+                    sd = backend.convert_to_csv(all_selected_ranges)
+                    download = st.download_button(
+                        label="Download data as CSV",
+                        data=sd,
+                        file_name='Результат.csv',
+                        mime='text/csv'
+                    )
+                if st.button("Очистить все диапазоны"):
+                    st.session_state["selected_ranges"] = []
                 st.write("Выбранные диапазоны:")
                 for i in range(1, len(selected_ranges) + 1):
                     st.write(f"Диапазон {i}:")
                     st.write(backend.chooses_ranges(df, selected_ranges[i - 1]))
-
-            # Удаляет со 2 нажатия, Егор исправь
-            if st.button("Очистить все диапазоны"):
-                st.session_state["selected_ranges"] = backend.clear_ranges(selected_ranges)
