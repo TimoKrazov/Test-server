0fc3da6e13c6b40e8cb2b64e5caaedf03b816654
Timo Krazov <83912804+TimoKrazov@users.noreply.github.com>
2024-05-20 12:50:47 +0800

    Update Об_приложении.py

--- NEW-PARS/Об_приложении.py
+++ NEW-PARS/Об_приложении.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 st.set_page_config(
     page_title="Главная страница",
-    page_icon= "🍺📟", layout="wide"
+    page_icon= "🍺", layout="wide"
 )
 
 st.markdown("""
